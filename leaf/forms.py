681fb447d73911a02a3d4c631bca206f07d0c1c6
vedantmahawar <63891053+vedantmahawar@users.noreply.github.com>
2023-03-14 16:39:01 -0700

    Delete leaf directory

--- leaf/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class SliderForm(forms.Form):
-    value = forms.IntegerField(min_value=1, max_value=10, label='How stressed are you, from 1-10? (10 being incredibly stressed)')
