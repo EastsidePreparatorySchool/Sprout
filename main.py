dd0c1c760ad558852fbf68f075af2b8723558aa5
chan-anni <119639326+chan-anni@users.noreply.github.com>
2023-05-10 10:16:35 -0700

    Testing logout
    I hope this works if not i will recall it

--- main.py
+++ main.py
@@ -3,11 +3,62 @@ from flask import Flask
 #Imports templates so we can use html
 from flask import render_template
 from flask import request
-from flask import Flask, render_template, redirect, url_for, session
+from flask import redirect, url_for, session, flash
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Hello'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///login_app.db'
+
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+# class 
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Invalid username or password', 'danger')
+
+    return render_template('login.html')
+
+# The logot thing
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', user=current_user)
 
 
+
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
 
 @app.route("/")
 def main():
