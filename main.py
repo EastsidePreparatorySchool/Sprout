06492f1ca3831964f46a4752127cf8a5ece19308
chan-anni <119639326+chan-anni@users.noreply.github.com>
2023-05-16 15:39:04 -0700

    Revert "sobbing im going to try to undo this"
    This reverts commit 39d45b3d6778e1a10f981da6a210730e05851f52.

--- main.py
+++ main.py
@@ -13,10 +13,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Hello'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///login_app.db'
 
-db = SQLAlchemy()
-login_manager = LoginManager()
-login_manager.init_app(app)
-##login_manager.login_view = 'login'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 # class 
 class User(UserMixin, db.Model):
@@ -24,77 +23,39 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
 
-db.init_app(app)
-with app.app_context(): db.create_all()
-
-#@login_manager.user_loader
-#def load_user(user_id):
-#    return User.query.get(int(user_id))
-
 @login_manager.user_loader
-def loader_user(user_id):
-    return Users.query.get(user_id)
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-@app.route('/register', methods=["GET", "POST"])
-def register():
-  # If the user made a POST request, create a new user
-    if request.method == "POST":
-        user = Users(username=request.form.get("username"),
-                     password=request.form.get("password"))
-        # Add the user to the database
-        db.session.add(user)
-        # Commit the changes made
-        db.session.commit()
-        # Once user account created, redirect them
-        # to login route (created later on)
-        return redirect(url_for("login"))
-    # Renders sign_up template if user made a GET request
-    return render_template("sign_up.html")
-
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if request.method == 'POST':
-#         username = request.form['username']
-#         password = request.form['password']
-#         user = User.query.filter_by(username=username).first()
-
-#         if user and check_password_hash(user.password, password):
-#             login_user(user)
-#             return redirect(url_for('dashboard'))
-#         else:
-#             flash('Invalid username or password', 'danger')
-
-#     return render_template('login.html')
-
-# # The logot thing
-# @app.route('/logout')
-# @login_required
-# def logout():
-#     logout_user()
-#     return redirect(url_for('login'))
-
-# @app.route('/dashboard')
-# @login_required
-# def dashboard():
-#     return render_template('dashboard.html', user=current_user)
-
-
-@app.route("/login", methods=["GET", "POST"])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    # If a post request was made, find the user by
-    # filtering for the username
-    if request.method == "POST":
-        user = Users.query.filter_by(
-            username=request.form.get("username")).first()
-        # Check if the password entered is the
-        # same as the user's password
-        if user.password == request.form.get("password"):
-            # Use the login_user method to log in the user
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+
+        if user and check_password_hash(user.password, password):
             login_user(user)
-            return redirect(url_for("home"))
-        # Redirect the user back to the home
-        # (we'll create the home route in a moment)
-    return render_template("login.html")
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Invalid username or password', 'danger')
+
+    return render_template('login.html')
+
+# The logot thing
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', user=current_user)
+
+
+
 
 if __name__ == '__main__':
     db.create_all()
